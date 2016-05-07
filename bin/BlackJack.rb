9e88bbefcf70790210bce235c23d13349d15bee3
ollieshmollie <oliverduncan@icloud.com>
2016-05-07 01:06:23 -0400

    Move essential programs to lib.

--- bin/BlackJack.rb
+++ bin/BlackJack.rb
@@ -1,34 +1,11 @@
 #!/usr/bin/env ruby
 require_relative '../lib/blackjack.rb'
-require_relative '../lib/gameboard.rb'
-system('printf "\e[8;25;92;t"')
+system('printf "\e[8;26;92;t"')
 
-LINEWIDTH = 92
+game = BlackJack.new
+game.play
 
-def play
-  system("clear")
-  puts "".ljust(LINEWIDTH,"=") 
-  puts "Welcome to BlackJack!!".rjust(LINEWIDTH/2 + 11)
-  puts "".ljust(LINEWIDTH," ")
-  print "Your hand:".ljust(LINEWIDTH/2) + "Dealer hand:".rjust(LINEWIDTH/2)
-  game = BlackJack.new
-  game.play
-  play_again
-end
 
-def play_again
-  puts "Play again?(Y/n)".rjust(LINEWIDTH/2 + 7)
-  input = gets
-  if input == "Y\n" || input == "\n" || input == "y\n"
-    play
-  elsif input == "n\n" || input == "N\n"
-    puts "Bye".rjust(LINEWIDTH/2 + 1)
-    puts "".ljust(LINEWIDTH,"=")
-  else
-    puts "Invaild input."
-    play_again
-  end
-end
 
-play
+
 
